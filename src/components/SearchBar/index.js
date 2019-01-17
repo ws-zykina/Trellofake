14184f08723e8a8abf2a804c2fec753ce927f997
Alex Moiseenko <alex.moisk@gmail.com>
2019-01-17 17:07:14 +0300

    update navigation styles

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -8,7 +8,7 @@ import './SearchBar.scss'
 function SearchBar({onSearch}) {
 	return (
 		<div className='search-bar-container'>
-			<Input name={'searchValue'} onChange={(name, e)=> onSearch(name, e)}/> />
+			<Input name={'searchValue'} onChange={(name, e)=> onSearch(name, e)}/>
 			<Icon iconName='fa-search' margin='0 8px'/>
 		</div>
 	)
